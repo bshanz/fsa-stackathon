b4cee240faa9da0141fcb802cb429868f43687b4
“Brian <brianshanley242@gmail.com>
2023-05-17 08:41:50 -0400

    Testing getting rid of config

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,14 +1,16 @@
 const Sequelize = require("sequelize");
 const config = {};
 
-require("dotenv").config();
-
 if (process.env.QUIET) {
   config.logging = false;
 }
+// const conn = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost/fsa_stackathon",
+//   config
+// );
+
 const conn = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/fsa_stackathon",
-  config
+  process.env.DATABASE_URL || "postgres://localhost/fsa_stackathon"
 );
 
 module.exports = conn;
