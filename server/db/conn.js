6787d2a147f533a1f2a1ab3fe60126b4a539cc0a
“Brian <brianshanley242@gmail.com>
2023-05-17 08:20:46 -0400

    Testing new deploy

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,10 +1,14 @@
-const Sequelize = require('sequelize');
-const config = {
-};
+const Sequelize = require("sequelize");
+const config = {};
 
-if(process.env.QUIET){
+require("dotenv").config();
+
+if (process.env.QUIET) {
   config.logging = false;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/fsa_stackathon', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/fsa_stackathon",
+  config
+);
 
 module.exports = conn;
